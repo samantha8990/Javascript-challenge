bbcfe8cac02188a6248fa34255faffb89c392c52
Samantha Sommer <samanthajeddy@gmail.com>
2021-06-16 20:12:16 -0700

    hw

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,3 +3,17 @@ var tableData = data;
 console.log(tableData);
 
 var tbody=d3.select("tbody");
+
+tableData.forEach(function(ufo) {
+    console.log(ufo);
+   
+    var row = tbody.append("tr");
+
+    
+    Object.entries(ufo).forEach(function([key, value]) {
+      console.log(key, value);
+   
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
